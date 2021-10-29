4039303e0146a1a9b23c559fd80112e2651b01b3
Niklas P Andersson <3985238+niklaspandersson@users.noreply.github.com>
2021-10-29 09:58:21 +0200

    Add placeholder for error handling

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,7 +1,26 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+const httpLink = new HttpLink({
+  uri: '/graphql',
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, response }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
