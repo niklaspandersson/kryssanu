424dd58e8e5f7c246443d4293ccb619ab37801f2
Niklas Andersson <3985238+niklaspandersson@users.noreply.github.com>
2023-04-26 21:11:11 +0200

    Implement bird details page

--- src/pages/bird/[id].tsx
+++ src/pages/bird/[id].tsx
@@ -1,13 +1,51 @@
+import styles from './[id].module.css';
+
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Bird } from '@prisma/client';
+import { api } from '~/utils/api';
+import Icon, { Binoculars } from '~/components/Icon';
+import IconHeader from '~/components/IconHeader';
+
+type Props = {
+  bird: Bird;
+};
+
+const Details: React.FC<Props> = ({ bird }) => {
+  const { data: observations } = api.birds.getObservations.useQuery({
+    birdId: bird.id,
+  });
+  return (
+    <>
+      <h3>{bird.swedish}</h3>
+      <img src="/bird.png" alt={`Bild av en ${bird.swedish}`} />
+      <caption>{'© Folke Foto'}</caption>
+      <IconHeader icon={<Binoculars />}>Observationer</IconHeader>
+      <ul className="observations">
+        {observations?.map(o => (
+          <li>{o.date.toDateString()}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 const BirdPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { data: bird } = api.birds.getOne.useQuery({
+    birdId: (Array.isArray(id) ? id[0] : id) ?? '',
+  });
 
   return (
-    <main>
-      <span>{id}</span>
+    <main className={styles.details}>
+      <header>
+        <Link href="/list">
+          <Icon name="close" />
+        </Link>
+      </header>
+      {bird && <Details bird={bird} />}
     </main>
   );
 };
